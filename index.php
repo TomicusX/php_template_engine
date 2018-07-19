1683409203ccfe9040cda27e6d53ac144a229caf
Maximillian Naza <maximillianaza@gmail.com>
2018-07-19 17:36:30 -0400

    Assign fav_food to template object

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 include 'Template.php';
 
 $template = new Template;
+
 $template->assign('username', '2Clutch');
 $template->assign('age', 23);
+$template->assign('fav_food', 'pasta')
+
 $template->render('temp');
 
 ?>
